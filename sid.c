f65735fb2d7a3d55c232765e693575283cfd33a2
Thomas Powell <twilliampowell@gmail.com>
2021-11-02 08:54:35 -0500

    8 octaves of notes

--- sid.c
+++ sid.c
@@ -6,13 +6,15 @@
 // /opt/homebrew/Cellar/cc65/2.19/share/cc65/include
 #define SID2     (*(struct __sid*)0xDE00)
 #define SID3     (*(struct __sid*)0xDF00)
-const int scale[] = {
+const unsigned int scale[] = {
   268,284,301,318,337,358,379,401,425,451,477,506,
   536,568,602,637,675,716,758,803,851,902,955,1012,
   1072,1136,1204,1275,1351,1432,1517,1607,1703,1804,1911,2025,
   2145,2273,2408,2551,2703,2864,3034,3215,3406,3608,3823,4050,
   4291,4547,4817,5103,5407,5728,6069,6430,6812,7217,7647,8101,
   8583,9094,9634,10207,10814,11457,12139,12860,13625,14435,15294,16203,
+  17167,18188,19269,20415,21629,22915,24278,25721,27251,28871,30588,32407U,
+  34334U,36376U,38539U,408330U,43258U,45830U,48556U,51443U,54502U,57743U,61176U,64814U
 };
 int scale_size = sizeof(scale) / sizeof(int);
 int main (void) {
@@ -50,7 +52,7 @@ int main (void) {
     SID3.v1.ctrl  = 0x11;	// Control voice 1
     SID3.v2.freq  = scale[(t+26)%scale_size];
     SID3.v2.ctrl  = 0x11;	// Control voice 1
-    SID3.v3.freq  = scale[(t+36)%scale_size];
+    SID3.v3.freq  = scale[(t-12)%scale_size];
     SID3.v3.ctrl  = 0x11;	// Control voice 1
 		for (i = 0 ; i < 1000; i++) {}
 		SID.v1.ctrl  = 0x10;
